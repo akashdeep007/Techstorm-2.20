1e0b7528aa0301d0712803018dbcf832430d20a7
Akashdeep Bhattacharya <36798161+akashdeep007@users.noreply.github.com>
2020-02-23 22:37:08 +0530

    Add files via upload

--- lib/Screens/EventPages/BrainPage.dart
+++ lib/Screens/EventPages/BrainPage.dart
@@ -1,19 +1,225 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
-import 'package:techstorm/Screens/EventPages/Carousal/BrainCarousal.dart';
+import 'package:techstorm/Screens/EventPages/FantaC.dart';
 
 
 
 
 class BrainPage extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
+    List<Widget> brainCards = [
+  //appmani
+  Container(
+    margin: EdgeInsets.only(left:10, right:10),
+    decoration: BoxDecoration(
+      image: DecorationImage(image: 
+      AssetImage('assets/images/BRAIN/APPMANIA.png',
+                  ), fit: BoxFit.cover)
+    ),
+    alignment: Alignment.center,
+    child: FlipCard(
+        flipOnTouch: true,
+        direction: FlipDirection.HORIZONTAL,
+        front: Stack(fit: StackFit.expand, children: <Widget>[
+          Center(
+              child: Text(
+            'APPMANIA',
+            style: TextStyle(
+                fontSize: 36, fontWeight: FontWeight.bold, color: Colors.white),
+          )),
+        ]),
+        back: Container(
+            color: Colors.white70,
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(20),
+            child: Column( mainAxisAlignment : MainAxisAlignment.spaceEvenly,children: <Widget>[
+              Column(
+                children: <Widget>[
+                                Text(
+                'APPMANIA',
+                style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              Text(
+                  'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum'),
+              
+                ],
+              ),
+RaisedButton(
+                onPressed: () {
+                  Navigator.push(context,new MaterialPageRoute(builder: (context) =>FantaC()));
+                },
+                child: const Text('Dive In', style: TextStyle(fontSize: 20)),
+              ),
+            ]))),
+  ),
+
+  //FANTAC
+  Container(
+    margin: EdgeInsets.only(left:10, right:10),
+    decoration: BoxDecoration(
+      image: DecorationImage(image: 
+      AssetImage('assets/images/BRAIN/FANTA-C.jpg',
+                  ), fit: BoxFit.cover)
+    ),
+    alignment: Alignment.center,
+    child: FlipCard(
+        flipOnTouch: true,
+        direction: FlipDirection.HORIZONTAL,
+        front: Stack(fit: StackFit.expand, children: <Widget>[
+          Center(
+              child: Text(
+            'FANTA-C',
+            style: TextStyle(
+                fontSize: 36, fontWeight: FontWeight.bold, color: Colors.white),
+          )),
+        ]),
+        back: Container(
+            color: Colors.white70,
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(20),
+            child: Column( mainAxisAlignment : MainAxisAlignment.spaceEvenly,children: <Widget>[
+              Column(
+                children: <Widget>[
+                                Text(
+                'Need for Speed',
+                style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              Text(
+                  'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum'),
+              
+                ],
+              ),
+RaisedButton(
+                onPressed: () {
+                  Navigator.push(context,new MaterialPageRoute(builder: (context) =>FantaC()));
+                },
+                child: const Text('Dive In', style: TextStyle(fontSize: 20)),
+              ),
+            ]))),
+  ),
+
+  //OMEGATRIX
+  Container(
+    margin: EdgeInsets.only(left:10, right:10),
+    decoration: BoxDecoration(
+      image: DecorationImage(image: 
+      AssetImage('assets/images/BRAIN/OMEGATRIX.jpg',
+                  ), fit: BoxFit.cover)
+    ),
+    alignment: Alignment.center,
+    child: FlipCard(
+        flipOnTouch: true,
+        direction: FlipDirection.HORIZONTAL,
+        front: Stack(fit: StackFit.expand, children: <Widget>[
+          Center(
+              child: Text(
+            'OMEGATRIX',
+            style: TextStyle(
+                fontSize: 36, fontWeight: FontWeight.bold, color: Colors.black),
+          )),
+        ]),
+        back: Container(
+            color: Colors.white70,
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(20),
+            child: Column( mainAxisAlignment : MainAxisAlignment.spaceEvenly,children: <Widget>[
+              Column(
+                children: <Widget>[
+                                Text(
+                'Omegatrix',
+                style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              Text(
+                  'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum'),
+              
+                ],
+              ),
+RaisedButton(
+                onPressed: () {
+                  Navigator.push(context,new MaterialPageRoute(builder: (context) =>FantaC()));
+                },
+                child: const Text('Dive In', style: TextStyle(fontSize: 20)),
+              ),
+            ]))),
+  ),
+
+  //TECHNOMANIA
+  Container(
+    margin: EdgeInsets.only(left:10, right:10),
+    decoration: BoxDecoration(
+      image: DecorationImage(image: 
+      AssetImage('assets/images/BRAIN/TECHNOMANIA.jpg',
+                  ), fit: BoxFit.cover)
+    ),
+    alignment: Alignment.center,
+    child: FlipCard(
+        flipOnTouch: true,
+        direction: FlipDirection.HORIZONTAL,
+        front: Stack(fit: StackFit.expand, children: <Widget>[
+          Center(
+              child: Text(
+            'TECHNOMANIA',
+            style: TextStyle(
+                fontSize: 36, fontWeight: FontWeight.bold, color: Colors.white),
+          )),
+        ]),
+        back: Container(
+            color: Colors.white70,
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(20),
+            child: Column( mainAxisAlignment : MainAxisAlignment.spaceEvenly,children: <Widget>[
+              Column(
+                children: <Widget>[
+                                Text(
+                'TECHNOMANIA',
+                style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              Text(
+                  'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum'),
+              
+                ],
+              ),
+RaisedButton(
+                onPressed: () {
+                  Navigator.push(context,new MaterialPageRoute(builder: (context) =>FantaC()));
+                },
+                child: const Text('Dive In', style: TextStyle(fontSize: 20)),
+              ),
+            ]))),
+  ),
+];
     return Scaffold(
-      appBar: AppBar(),
-      body: CarouselSlider.builder(height: 400,
-   itemCount: BrainCards.length,
+      appBar: AppBar(
+        backgroundColor: Colors.black87,
+        title: Text('Brain'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage('assets/images/back.gif'), fit: BoxFit.cover)
+        ),
+        alignment: Alignment.center,
+        child: CarouselSlider.builder(
+          enableInfiniteScroll: true,
+          height: 500,
+   itemCount: brainCards.length,
    itemBuilder: (BuildContext context, int itemIndex) =>
-        BrainCards[itemIndex]
-   ));
+          brainCards[itemIndex]
+   ),
+      ));
   }
 }
