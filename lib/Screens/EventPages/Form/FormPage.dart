c45fe43f55a71268a2b3dbb9b58ec70b5259af18
Radiumskull <whoisaritra@gmail.com>
2020-03-06 00:50:11 +0530

    Team Form and Single Form Fixed.

--- lib/Screens/EventPages/Form/FormPage.dart
+++ lib/Screens/EventPages/Form/FormPage.dart
@@ -99,100 +99,21 @@ class _RegisterFormState extends State<RegisterForm> {
                           },
                         ),
                         SizedBox(height: 20,),
-                        DropDownFormField(                          
-                        validator: (value) {
-                            if(value == null){
-                              return 'Select Department';
+                TextFormField(
+                      validator: (text) {
+                            if(text.isEmpty){
+                              return 'Enter Department';
                             }
                             return null;
                           },
-                    
-                    titleText: 'Department',
-                    hintText: 'Please choose one',
-                    value: department,
-                    onSaved: (value) {
-                      setState(() {
-                        department= value;
-                      });
-                    },
-                    onChanged: (value) {
-                      setState(() {
-                        department = value;
-                      });
-                    },
-                    dataSource: [
-                      {
-                        "display": "IT",
-                        "value": "IT",
-                      },
-                      {
-                        "display": "CSE",
-                        "value": "CSE",
-                      },
-                      {
-                        "display": "ECE",
-                        "value": "ECE",
-                      },
-                      {
-                        "display": "EE",
-                        "value": "EE",
-                      },
-                      {
-                        "display": "MCA",
-                        "value": "MCA",
-                      },
-                      {
-                        "display": "BCA",
-                        "value": "BCA",
-                      },
-                    ],
-                    textField: 'display',
-                    valueField: 'value',
+                          decoration: InputDecoration(
+                            labelText: 'Department'
+                          ),
+                          onChanged: (text) {
+                            department = text;
+                          },                  
                 ),
-
-                        SizedBox(height: 20,),
-                        DropDownFormField(
-                        validator: (value) {
-                            if(value == null){
-                              return 'Select Year';
-                            }
-                            return null;
-                          },
-                    titleText: 'Year',
-                    hintText: 'Please choose one',
-                    value: year,
-                    onSaved: (value) {
-                      setState(() {
-                        department = value;
-                      });
-                    },
-                    onChanged: (value) {
-                      setState(() {
-                        year = value;
-                      });
-                    },
-                    dataSource: [
-                      {
-                        "display": "1st Year",
-                        "value": "1st Year",
-                      },
-                      {
-                        "display": "2nd Year",
-                        "value": "2nd Year",
-                      },
-                      {
-                        "display": "3rd Year",
-                        "value": "3rd Year",
-                      },
-                      {
-                        "display": "4th Year",
-                        "value": "4th Year",
-                      },
-                    ],
-                    textField: 'display',
-                    valueField: 'value',
-                ),
-                SizedBox(height: 40,),
+                SizedBox(height: 20,),
                   TextFormField(
                       validator: (text) {
                             if(text.isEmpty){
@@ -224,16 +145,30 @@ class _RegisterFormState extends State<RegisterForm> {
                         ) : Container(),
                         Text(error, style: TextStyle(color: Colors.red, fontSize: 16, fontWeight: FontWeight.bold),),
                         FlatButton(
+                          child: Container(alignment: Alignment.center,height : 40, width: 200,color: Colors.red,  child:Container(alignment: Alignment.center,height : 40, width: 200,color: Colors.red,  child: Text('Register', style: TextStyle(fontSize: 24, fontWeight:FontWeight.bold),),),),
                           onPressed: () {
                             final database = FirebaseDatabase.instance.reference();
+                                                      if(_formKey.currentState.validate()){
+                              showDialog(
+                                      context: context,
+                                      builder: (BuildContext context){
+                                          return AlertDialog(
+                                            title: Text("Confirm Registration"),
+                                            content: Text("Press Confirm to Generate QR Code"),
+                                            actions: <Widget>[
+                                              RaisedButton(
+                                                child: Text('Confirm'),
+                                                onPressed: () { 
                           setState(() {
                             error = '';
                           });
-                          if(_formKey.currentState.validate()){
+
                             String path = widget.eventType + '/' + widget.eventName + '/' + contact;
                             database.reference().child(path).once().then((DataSnapshot dataSnapshot) {
                               print(dataSnapshot.value);
                               if (dataSnapshot.value == null){
+                                                                    String data = "${widget.eventType} ${widget.eventName} $contact";
+                                    Navigator.push(context,new MaterialPageRoute(builder: (context) =>QrGen(widget.eventName, data)));
                                 database.child(widget.eventType + '/' + widget.eventName + '/' + contact).set({
                                   'a_teamName' : '',
                                   'e_email' : email,
@@ -244,22 +179,7 @@ class _RegisterFormState extends State<RegisterForm> {
                                   'payment' : 'false',
                                   'time' : DateTime.now().toString(),                           
                                 });
-                                    String data = "${widget.eventType} ${widget.eventName} $contact";
-                                      showDialog(
-                                      context: context,
-                                      builder: (BuildContext context){
-                                          return AlertDialog(
-                                            title: Text("Confirm Registration"),
-                                            content: Text("Press Confirm to Generate QR Code"),
-                                            actions: <Widget>[
-                                              RaisedButton(
-                                                child: Text('Confirm'),
-                                                onPressed: () => Navigator.push(context,new MaterialPageRoute(builder: (context) =>QrGen(widget.eventName, data))),
-                                              ),
-                                            ],
-                                          );
-                                      }
-                                    );
+
 
                                 // Navigator.push(context,new MaterialPageRoute(builder: (context) =>QrGen(widget.eventName, data)));
                               }
@@ -267,12 +187,17 @@ class _RegisterFormState extends State<RegisterForm> {
                                 setState(() => error = 'Already Registered');
                               }
                             });
-                            
-                          } else {
-                            setState(() => error = '');
-                          }
                         },
-                        child: Container(alignment: Alignment.center,height : 40, width: 200,color: Colors.red,  child:Container(alignment: Alignment.center,height : 40, width: 200,color: Colors.red,  child: Text('Register', style: TextStyle(fontSize: 24, fontWeight:FontWeight.bold),),),),)
+                                                  
+                                              ),
+                                            ],
+                                          );
+                                      }
+                                    );
+                                                      }
+                            
+
+                          })
                       ],
                     ),
                   ),
